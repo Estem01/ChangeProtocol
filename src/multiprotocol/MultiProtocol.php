6d51566f1055e1c78aeb733a8ad0330829238403
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-06-18 17:52:59 -0300

    Update MultiProtocol.php

--- src/multiprotocol/MultiProtocol.php
+++ src/multiprotocol/MultiProtocol.php
@@ -34,12 +34,13 @@ class MultiProtocol extends PluginBase implements Listener {
         if(!$pk instanceof LoginPacket) {
             return;
         }
+        $name = $event->getUsername();
         $player = $event->getOrigin();
         $currentProtocol = ProtocolInfo::CURRENT_PROTOCOL;
 
         if($pk->protocol !== $currentProtocol) {
             $pk->protocol = $currentProtocol;
-            $this->getLogger()->alert("§6{$player->getName()}'s protocol changed to {$currentProtocol}!");
+            $this->getLogger()->alert("§6{$player->$name}'s protocol changed to {$currentProtocol}!");
         }
     }
 }
