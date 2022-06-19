09707a769901a8cd1b39d6005235a420180fbb8f
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-06-18 23:22:22 -0300

    Update MultiProtocol.php

--- src/multiprotocol/MultiProtocol.php
+++ src/multiprotocol/MultiProtocol.php
@@ -24,12 +24,13 @@ class MultiProtocol extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
         @mkdir($this->getDataFolder());
-	$this->acceptProtocol = (New Config($this->getDataFolder()."accept.yml", Config::YAML))->get("accept-protocol");
+	$this->acceptProtocol = (new Config($this->getDataFolder()."accept.yml", Config::YAML))->get("accept-protocol");
         $saveResources(accpet.yml);
         $config = getConfig(accpet.yml);
 
         if ($this->acceptProtocol === false || empty($this->acceptProtocol)) : void{
-			if $this->acceptProtocol[] = ProtocolInfo::CURRENT_PROTOCOL;
+
+			$this->(acceptProtocol[] = (ProtocolInfo::CURRENT_PROTOCOL));
 			$config = new Config($this->getDataFolder()."accept.yml", Config::YAML);
 			$config->set("accept-protocol", [ProtocolInfo::CURRENT_PROTOCOL]);
 			$config->save();
