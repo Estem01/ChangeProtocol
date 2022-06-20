17978d70ace730cadfebcf30b71fda1824948e8c
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-06-20 05:32:40 -0300

    Update and rename src/changeprotocol/ChangeProtocol.php to src/Estem0/ChangeProtocol.php

--- src/Estem0/ChangeProtocol.php
+++ src/Estem0/ChangeProtocol.php
@@ -21,7 +21,6 @@ class ChangeProtocol extends PluginBase implements Listener {
 
     public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("§3MultiProtocol_1.0.2 Only 1.19 versions Enable");
 
 		$this->acceptProtocol = [534, 533, 532, 530, 526, 524, 516, 514, 512];
     }
